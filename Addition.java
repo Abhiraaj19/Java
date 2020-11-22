6fb6bd936e02522e0efb119fcdbf922712d2095b
Abhishek-Gupta-lab <74490200+Abhishek-Gupta-lab@users.noreply.github.com>
2020-11-23 00:05:30 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition
+{
+    public static void main(String args[]) 
+    {
+      int a=10;
+      int b=25;
+      int c=a+b;
+
+      System.out.println("Sum of a+b = " + c);
+    }
+}
