39b29140451c219bc0dd1c3f6ff3083e71d7cc93
Abhishek-Gupta-lab <74490200+Abhishek-Gupta-lab@users.noreply.github.com>
2020-11-29 21:46:51 +0530

    Add files via upload

---
+++ Additionofnumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Additionofnumbers
+{
+    public static void main(String args[]) 
+    {
+      Scanner inp = new Scanner(System.in);
+      int x,y;
+      System.out.println("Addition of numbers");
+      x=inp.nextInt();
+      y=inp.nextInt();
+      System.out.println("Sum:"+(x+y));
+    }
+}
